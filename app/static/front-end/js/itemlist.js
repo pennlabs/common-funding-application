2389d3d5eb5244b037c74f44e24a6cc207cabb75
Kevin Su <kevinsu@seas.upenn.edu>
2012-02-23 03:17:05 -0500

    changed static content paths, implemented regex for dollar field verification, fixed numericOnly js to allow "." (periods)

--- app/static/front-end/js/itemlist.js
+++ app/static/front-end/js/itemlist.js
@@ -24,17 +24,20 @@ function numericOnly(event) {
   console.log(event.keyCode);
   
   // Allow: backspace, delete, tab and escape
-  if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || 
-    // Allow: Ctrl+A
-    (event.keyCode == 65 && event.ctrlKey === true) || 
-    // Allow: home, end, left, right
-    (event.keyCode >= 35 && event.keyCode <= 39)) {
-    // let it happen, don't do anything
-    return;
+  if (event.keyCode == 46 || event.keyCode == 8 ||
+      event.keyCode == 9 || event.keyCode == 27 || 
+      // Allow: Ctrl+A
+      (event.keyCode == 65 && event.ctrlKey === true) || 
+      // Allow: home, end, left, right
+      (event.keyCode >= 35 && event.keyCode <= 39)) {
+      // let it happen, don't do anything
+      return;
   }
   else {
     // Ensure that it is a number and stop the keypress
-    if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+    if ((event.keyCode < 48 || event.keyCode > 57) &&
+	(event.keyCode < 96 || event.keyCode > 105) &&
+	(event.keyCode != 190)) {
         event.returnValue = false;
         event.preventDefault();
     }   
