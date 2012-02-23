9b7a0e11ee90833249a5cce2d7f7a5026fe28dde
Kevin Su <kevinsu@seas.upenn.edu>
2012-02-22 23:19:13 -0500

    changed static file linking to use builtin template tags

--- apache/django.wsgi
+++ apache/django.wsgi
@@ -1,9 +1,11 @@
 import os, sys
 
+import django.core.handlers.wsgi
+
+
 PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(PROJECT_ROOT)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ from sandbox_config import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -107,7 +107,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_PATH, "static/front-end"),
+    os.path.join(PROJECT_ROOT, "static/front-end"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
