66c90c9a5e4d0fc3f9bd6313557dbcedd3f0cf3e
Ceasar Bautista <ceasarb@seas.upenn.edu>
2012-04-05 20:00:21 -0400

    Added secret keys to events so they can be shared.

--- app/views/views.py
+++ app/views/views.py
@@ -17,30 +17,47 @@ from app.models import Event, EligibilityQuestion, EligibilityAnswer, \
     CommonFreeResponseQuestion, CommonFreeResponseAnswer
 from app.helpers import save_event
 
+
+NOT_AUTHORIZED = 'app.views.index'
+
 def index(request):
     return redirect('app.views.events')
 
 
-def creator_or_funder(view):
+def authorization_required(view):
+  """Ensure only a permitted user can access an event.
+  A user is permitted if:
+    * They requested the event
+    * They are a funder
+    * They have a secret key to an event
+  """
   def protected_view(request, event_id, *args, **kwargs):
-    user = request.user
+    user = request.user.get_profile()
     event = Event.objects.get(pk=event_id)
-    if user.cfauser.is_funder or user.cfauser.requested(event):
+    if user.is_funder or user.requested(event):
       return view(request, event_id, *args, **kwargs)
     else:
-      return redirect('app.views.index') # not authorized
+      try:
+        key = request.GET['key']
+      except KeyError:
+        return redirect(NOT_AUTHORIZED)
+      else:
+        if key == event.secret_key:
+          return view(request, event_id, *args, **kwargs)
+        else:
+          return redirect(NOT_AUTHORIZED)
   return protected_view
 
 
-def authorization_required(view):
-  """Ensure the user requested the event."""
+def requester_only(view):
+  """Ensure only the user who requested the event can access a page."""
   def protected_view(request, event_id, *args, **kwargs):
     user = request.user
     event = Event.objects.get(pk=event_id)
     if user.get_profile().requested(event):
       return view(request, event_id, *args, **kwargs)
     else:
-      return redirect('app.views.index') # not authorized
+      return redirect(NOT_AUTHORIZED)
   return protected_view
 
 
@@ -91,7 +108,7 @@ def event_new(request):
 
 
 @login_required
-@authorization_required
+@requester_only
 def event_edit(request, event_id):
   user = request.user
   if request.method == 'GET':
@@ -105,7 +122,7 @@ def event_edit(request, event_id):
 
 
 @login_required
-@creator_or_funder
+@authorization_required
 def event_show(request, event_id):
   user = request.user
   event = Event.objects.get(pk=event_id)
@@ -181,7 +198,7 @@ def event_show(request, event_id):
     return HttpResponseNotAllowed(['POST'])
 
 @login_required
-@authorization_required
+@requester_only
 def items(request, event_id):
   user = request.user
   if request.method == 'POST':
@@ -246,7 +263,7 @@ def itemlist_funder(request, event_id):
 
 
 @login_required
-@authorization_required
+@requester_only
 def event_destroy(request, event_id):
   event = Event.objects.get(pk=event_id)
   event.delete()
@@ -254,7 +271,7 @@ def event_destroy(request, event_id):
 
 
 @login_required
-@authorization_required
+@requester_only
 def free_response(request, event_id, funder_id):
   """Show the free response form for a funder and process it."""
   user = request.user
--- urls.py
+++ urls.py
@@ -5,6 +5,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.urls')),
-    url(r'^application/(\d+)/', 'app.views.submitted'),
     url(r'', include('app.urls')),
 )
