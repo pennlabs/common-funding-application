73f5c5348c1c650e5a47268ade199b20d9af443f
Greg Terrono <gterrono@seas.upenn.edu>
2012-04-19 21:48:25 -0400

    Entire application is disabled after funding

--- app/templatetags/widgets.py
+++ app/templatetags/widgets.py
@@ -34,10 +34,10 @@ def fundingbar(context, totalAmount, fundDict):
   return render_to_string('app/templatetags/fundingbar.html', new_context)
 
 
-@tag(register, [Variable()])
-def itemlist_requester(context, items):
+@tag(register, [Variable(), Variable()])
+def itemlist_requester(context, items, funded):
   # takes a dictionary of items
-  new_context = {'items':items}
+  new_context = {'items':items, 'funded':funded}
   return render_to_string('app/templatetags/itemlist-requester.html', new_context)
 
 
