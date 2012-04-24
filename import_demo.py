fa06b008c462c5d330b678b0b9d32a8d5320dba3
Kevin Su <kevinsu@seas.upenn.edu>
2012-04-23 20:34:19 -0400

    updated Event model to remove USPhoneNumberField, added contact_email and contact_phone to Event model, updated import script for new models

--- import_demo.py
+++ import_demo.py
@@ -85,21 +85,37 @@ def import_events():
                          date=datetime.date(year=2012,
                                             month=4,
                                             day=1),
+                         time=datetime.time(hour=12),
+                         location='Huntsman',
+                         anticipated_attendance=0,
+                         admission_fee='0',
                          requester=CFAUser.objects.get(user=User.objects.get(username="testrequester1")))
     Event.objects.create(name="testrequester1's Second Event",
                          date=datetime.date(year=2012,
                                             month=4,
                                             day=2),
+                         time=datetime.time(hour=13),
+                         location='Moore 100',
+                         anticipated_attendance=50,
+                         admission_fee='69.69',
                          requester=CFAUser.objects.get(user=User.objects.get(username="testrequester1")))
     Event.objects.create(name="testrequester2's First Event",
                          date=datetime.date(year=2012,
                                             month=5,
                                             day=1),
+                         time=datetime.time(hour=14),
+                         location='DRLB',
+                         anticipated_attendance=30,
+                         admission_fee='10',
                          requester=CFAUser.objects.get(user=User.objects.get(username="testrequester2")))
     Event.objects.create(name="testrequester2's Second Event",
                          date=datetime.date(year=2012,
                                             month=5,
                                             day=2),
+                         time=datetime.time(hour=20),
+                         location='Rodin',
+                         anticipated_attendance=10,
+                         admission_fee='8',
                          requester=CFAUser.objects.get(user=User.objects.get(username="testrequester2")))
 
 def import_sites():
