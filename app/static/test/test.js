97202a5ad24823bd89d7859f170d4e0d267f31da
Geoff Vedernikoff <yefim323@gmail.com>
2012-09-26 20:57:36 -0400

    Added tests for addExpectation

--- app/static/test/test.js
+++ app/static/test/test.js
@@ -8,4 +8,33 @@ describe('Recommendations', function(){
       assert.ok( _.isEmpty( recs.addExpectation([], []) ) );
     })
   })
+  describe("#getRecommended()", function() {
+    it("should be empty array", function() {
+      assert.ok( _.isEmpty( recs.getRecommended({}) ) );
+    })
+  })
+  describe("#checkExpectations()", function() {
+    it("should be empty object", function() {
+      assert.ok( _.isEmpty( recs.checkExpectations(null, {}) ) );
+    })
+  })
+  describe("#addExpectation()", function() {
+    it("should add positive expectation", function() {
+      var E = {};
+      var y = ["1"];
+      E = recs.addExpectation(y, []);
+      var expected = {1: true};
+      assert.ok( _.isEqual(E, expected) );
+    })
+  })
+  describe("#addExpectation()", function() {
+    it("should add two expectations", function() {
+      var E = {};
+      var y = ["1"];
+      var n = ["2"];
+      E = recs.addExpectation(y, n);
+      var expected = {1: true, 2: false};
+      assert.ok( _.isEqual(E, expected) );
+    })
+  })
 })
