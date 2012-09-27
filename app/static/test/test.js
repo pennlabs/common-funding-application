2ee69817b435e9abb8c1604fa1dbda3def6d58f1
Geoff Vedernikoff <yefim323@gmail.com>
2012-09-26 21:27:26 -0400

    Added tests to check expectations against reality

--- app/static/test/test.js
+++ app/static/test/test.js
@@ -29,12 +29,35 @@ describe('Recommendations', function(){
   })
   describe("#addExpectation()", function() {
     it("should add two expectations", function() {
-      var E = {};
       var y = ["1"];
       var n = ["2"];
-      E = recs.addExpectation(y, n);
+      var E = recs.addExpectation(y, n);
       var expected = {1: true, 2: false};
       assert.ok( _.isEqual(E, expected) );
+    })
+  })
+  describe("#checkRelations()", function() {
+    it("should remain the same", function() {
+      var y = ["1"];
+      var E = recs.addExpectation(y, []);
+      var expected = {1: true};
+      assert.ok( _.isEqual(E, expected) );
+
+      var is_checked = true;
+      var reality = {1: true};
+      assert.ok( _.isEqual(recs.checkExpectations(is_checked, E), reality ) );
+    })
+  })
+  describe("#checkRelations()", function() {
+    it("should differ from reality", function() {
+      var y = ["1"];
+      var E = recs.addExpectation(y, []);
+      var expected = {1: true};
+      assert.ok( _.isEqual(E, expected) );
+
+      var is_checked = false;
+      var reality = {1: false};
+      assert.ok( _.isEqual(recs.checkExpectations(is_checked, E), reality ) );
     })
   })
 })
