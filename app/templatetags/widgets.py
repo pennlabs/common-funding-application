184bc3dba53fb0fb1ee27434f718edd1a7530cf0
David Xu <davidxu@ve.rckr5ngx.vesrv.com>
2012-10-03 18:30:20 -0400

    categories being loaded from db

--- app/templatetags/widgets.py
+++ app/templatetags/widgets.py
@@ -37,7 +37,7 @@ def fundingbar(context, totalAmount, fundDict):
 @tag(register, [Variable(), Variable()])
 def itemlist_requester(context, items, funded):
   # takes a dictionary of items
-  new_context = {'items':items, 'funded':funded}
+  new_context = {'items':items, 'funded':funded, 'CATEGORIES': CATEGORIES}
   return render_to_string('app/templatetags/itemlist-requester.html', new_context)
 
 
