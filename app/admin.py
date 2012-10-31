7e5ee99a42cf171729e9a27417089894d3b56ec1
Ceasar Bautista <cbautista2010@gmail.com>
2012-10-31 18:27:34 -0400

    Add user-type to cfauser admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+
 from models import (CFAUser, CommonFreeResponseQuestion, EligibilityQuestion,
                     FreeResponseQuestion, FunderConstraint)
 
 
-admin.site.register(CFAUser)
+class CFAUserAdmin(admin.ModelAdmin):
+    list_display = ('user_type', 'osa_email')
+
+
+admin.site.register(CFAUser, CFAUserAdmin)
 admin.site.register(EligibilityQuestion)
 admin.site.register(CommonFreeResponseQuestion)
 admin.site.register(FreeResponseQuestion)
