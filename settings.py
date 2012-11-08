1e79dfed67e1ed144415441227727dcd5287e101
Kyle Hardgrave <kyle@kylehardgrave.com>
2012-11-07 20:36:52 -0500

    Remove deprecated ADMIN_MEDIA_PREFIX

--- settings.py
+++ settings.py
@@ -79,12 +79,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = os.path.join(SITE_NAME, 'static/')
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = os.path.join(URL_ROOT, 'static/admin/')
+STATIC_URL = os.path.join(URL_ROOT, 'static/')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
