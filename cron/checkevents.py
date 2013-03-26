b3e365168222bca995e2d1450785743b52c62dad
Geoffrey Vedernikoff <yefim323@gmail.com>
2013-03-25 21:32:32 -0400

    Cron job marks correctly

--- cron/checkevents.py
+++ cron/checkevents.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 """Checks whether an event is over and sends emails the requester if so"""
 
-import os, sys
+import os
+import sys
 from datetime import date, timedelta
 
-PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+PROJECT_ROOT = os.path.realpath(
+    os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(PROJECT_ROOT)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 from app.models import Event
 
-DAYS = 3 # number of days to wait until sending an email
+DAYS = 3  # number of days to wait until sending an email
 now = date.today()
 events = Event.objects.all()
 
 for event in events:
-  if not event.over:
+  if not event.followup_needed:
     then = event.date
     if (now - then) > timedelta(days=DAYS):
-      event.over = True
+      event.status = 'W'
       event.save()
       event.notify_requester_for_followups()
       print "%s has been over for more than 3 days" % event.name
