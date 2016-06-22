f413a57a7522233b3f48b45277b6683f42fc87db
Adel Qalieh <aqalieh95@gmail.com>
2016-06-22 03:45:05 -0400

    Start testing views as well

--- app/tests.py
+++ app/tests.py
@@ -11,3 +11,19 @@ class CFAUserTest(TestCase):
     def test_is_requester(self):
         cuser = CFAUser.objects.get(pk=11)
         self.assertTrue(cuser.is_requester)
+
+
+class TestViews(TestCase):
+    def test_index(self):
+        """Test to see that index redirects to login page
+
+        TODO: In the future, index page should be the login page
+        """
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_login_page(self):
+        resp = self.client.get('/accounts/login/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('form' in resp.context)
+        self.assertContains(resp, 'Login')
