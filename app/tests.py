e26428eb42a4da13328d68aa563107d8e4468235
Adel Qalieh <aqalieh95@gmail.com>
2016-06-22 13:27:28 -0400

    Add tests for login views

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
 from .models import CFAUser
 
 
 class CFAUserTest(TestCase):
+    fixtures = ['cfausers.json']
+
     def test_is_funder(self):
         cuser = CFAUser.objects.get(pk=1)
         self.assertTrue(cuser.is_funder)
@@ -27,3 +30,28 @@ class TestViews(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('form' in resp.context)
         self.assertContains(resp, 'Login')
+
+
+class TestLoginViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='philo',
+                                             email='philo@upenn.edu',
+                                             password='we<3literature')
+
+    def test_index(self):
+        self.client.login(username='philo', password='we<3literature')
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Current Applications')
+
+    def test_index_not_logged_in(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 302)
+
+    def test_logout(self):
+        self.client.login(username='philo', password='we<3literature')
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.client.logout()
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 302)
