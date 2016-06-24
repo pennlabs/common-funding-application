01267b438991d4819bb0b25c4f1044b03ecb19c3
Adel Qalieh <aqalieh95@gmail.com>
2016-06-24 03:14:14 -0400

    YAPF helpers

--- app/templatetags/helpers.py
+++ app/templatetags/helpers.py
@@ -2,19 +2,22 @@ from app.models import Grant
 
 
 def funders_grant_data_to_item(context, item, funder_id):
-  grant = Grant.objects.filter(item=item, funder__id=funder_id)
-  return (grant[0].amount, item.id) if grant else (None, item.id)
+    grant = Grant.objects.filter(item=item, funder__id=funder_id)
+    return (grant[0].amount, item.id) if grant else (None, item.id)
+
 
 def funder_item_data(context, item, funders):
-  funders_data = []
-  for funder in funders:
-    grant_amount, grant_id = funders_grant_data_to_item(context, item, funder.id)
-    funders_data.append((funder.id, grant_amount, grant_id))
-  return (item, funders_data)
+    funders_data = []
+    for funder in funders:
+        grant_amount, grant_id = funders_grant_data_to_item(context, item,
+                                                            funder.id)
+        funders_data.append((funder.id, grant_amount, grant_id))
+    return (item, funders_data)
+
 
 def get_or_none(model, **kwargs):
-  """Get an object, or None."""
-  try:
-    return model.objects.get(**kwargs)
-  except model.DoesNotExist:
-    return None
+    """Get an object, or None."""
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
