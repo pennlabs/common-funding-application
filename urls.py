4e72bbeb394928fb2f6a51560f92f3c227365509
Adel Qalieh <aqalieh95@gmail.com>
2016-06-25 19:01:19 -0400

    Remove deprecated urls import

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls.defaults import include, patterns, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'', include('app.urls')),
