58ed9d7c82921d4e8771ecdb3bb72c27d326f87b
Adel Qalieh <aqalieh95@gmail.com>
2016-10-03 17:09:22 -0400

    Switch to model_activation urls for registration

--- urls.py
+++ urls.py
@@ -5,6 +5,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('registration.backends.model_activation.urls')),
     url(r'', include('app.urls')),
 )
