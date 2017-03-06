0624fb7e66a7907088dd6d2b5bd930875d4e8822
Adel Qalieh <aqalieh95@gmail.com>
2017-03-06 12:13:15 -0500

    Add Procfile with gunicorn

--- apache/wsgi.py
+++ apache/wsgi.py
@@ -1,13 +1,14 @@
 import os
 import sys
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(PROJECT_ROOT)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
