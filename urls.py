9e4513d3510d52961a3a7c0042bdbe21e014c633
Adel Qalieh <aqalieh95@gmail.com>
2017-06-19 02:12:16 -0400

    Use new URL includes

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.model_activation.urls')),
     url(r'', include('app.urls')),
 ]
