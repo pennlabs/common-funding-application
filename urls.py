af7fc8ff69ecfab03f99b9958491234ca93ded02
Eric Wang <ezwang1@gmail.com>
2018-02-15 09:32:42 -0500

    custom admin header

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
+admin.site.site_header = "CFA administration"
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.model_activation.urls')),
