817abd2c3924129d8eb78cbb2dc1a920c8f709db
Eric Wang <ezwang1@gmail.com>
2018-03-08 07:48:49 -0500

    better password reset form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import PasswordChangeForm as BasePasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm as BasePasswordChangeForm, PasswordResetForm as BasePasswordResetForm
 from django.core.validators import RegexValidator
 from registration.forms import RegistrationForm as BaseRegistrationForm
 
@@ -11,7 +11,7 @@ class RegistrationForm(BaseRegistrationForm):
         kwargs["label_suffix"] = ""
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields["email"].label = "Penn Email Address"
-        self.fields["email"].help_text = "Required. Email address ending with '.upenn.edu'"
+        self.fields["email"].help_text = "Required. Email address ending with 'upenn.edu'"
         self.fields["email"].validators.append(RegexValidator(r"^.+@([a-zA-Z]*\.)?upenn\.edu$", "Enter a valid Penn email."))
         for f in self.fields.values():
             f.widget.attrs.update({"class": "form-control"})
@@ -21,6 +21,16 @@ class PasswordChangeForm(BasePasswordChangeForm):
     def __init__(self, *args, **kwargs):
         kwargs["label_suffix"] = ""
         super(PasswordChangeForm, self).__init__(*args, **kwargs)
+        for f in self.fields.values():
+            f.widget.attrs.update({"class": "form-control"})
+
+
+class PasswordResetForm(BasePasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        kwargs["label_suffix"] = ""
+        super(PasswordResetForm, self).__init__(*args, **kwargs)
+        self.fields["email"].label = "Penn Email Address"
+        self.fields["email"].validators.append(RegexValidator(r"^.+@([a-zA-Z]*\.)?upenn\.edu$", "Enter a valid Penn email."))
         for f in self.fields.values():
             f.widget.attrs.update({"class": "form-control"})
 
--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from registration.backends.model_activation.views import RegistrationView
-from django.contrib.auth.views import PasswordChangeView
-from app.forms import RegistrationForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView
+from app.forms import RegistrationForm, PasswordChangeForm, PasswordResetForm
 
 admin.autodiscover()
 
@@ -12,6 +12,7 @@ admin.site.site_header = "CFA administration"
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/password/change/$', PasswordChangeView.as_view(form_class=PasswordChangeForm, success_url="done"), name='auth_password_change'),
+    url(r'^accounts/password/reset/$', PasswordResetView.as_view(form_class=PasswordResetForm, success_url="done"), name='auth_password_reset'),
     url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationForm), name='registration_register'),
     url(r'^accounts/', include('registration.backends.model_activation.urls')),
     url(r'', include('app.urls')),
