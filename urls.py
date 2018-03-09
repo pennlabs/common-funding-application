04222faf0e218ea309a848bd3a0fcdbc191bece6
Eric Wang <ezwang1@gmail.com>
2018-03-09 11:06:53 -0500

    fix password reset redirect

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from registration.backends.model_activation.views import RegistrationView
-from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
 from app.forms import RegistrationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 
 admin.autodiscover()
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         PasswordResetConfirmView.as_view(form_class=SetPasswordForm),
         name='password_reset_confirm'),
+    url(r'^accounts/password/reset/complete/$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationForm), name='registration_register'),
     url(r'^accounts/', include('registration.backends.model_activation.urls')),
     url(r'', include('app.urls')),
