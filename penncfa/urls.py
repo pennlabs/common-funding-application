592a5d7a6fc562baeef91a420d675b5adebd70fe
Thomas Ngulube <47449914+Porcupine1@users.noreply.github.com>
2025-02-21 23:19:17 -0500

    update depricated code

--- penncfa/urls.py
+++ penncfa/urls.py
@@ -1,25 +1,24 @@
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from django.contrib import admin
 
 from django_registration.backends.activation.views import RegistrationView
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
 from app.forms import RegistrationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 
-
 admin.autodiscover()
 
 admin.site.site_header = "CFA administration"
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/password/change/$', PasswordChangeView.as_view(form_class=PasswordChangeForm), name='auth_password_change'),
-    url(r'^accounts/password/reset/$', PasswordResetView.as_view(form_class=PasswordResetForm), name='auth_password_reset'),
-    url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path('admin/', admin.site.urls),
+    path('accounts/password/change/', PasswordChangeView.as_view(form_class=PasswordChangeForm), name='auth_password_change'),
+    path('accounts/password/reset/', PasswordResetView.as_view(form_class=PasswordResetForm), name='auth_password_reset'),
+    re_path(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         PasswordResetConfirmView.as_view(form_class=SetPasswordForm),
         name='password_reset_confirm'),
-    url(r'^accounts/password/reset/complete/$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationForm), name='registration_register'),
-    url(r'^accounts/', include('django_registration.backends.activation.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'', include('app.urls')),
+    path('accounts/password/reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('accounts/register/', RegistrationView.as_view(form_class=RegistrationForm), name='registration_register'),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('app.urls')),
 ]
